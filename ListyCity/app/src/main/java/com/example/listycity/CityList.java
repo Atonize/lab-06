34ae0f32cab70f8e4484281b6a1eb7c3aabb6002
Atonize <aamalia@ualberta.ca>
2026-02-10 18:39:59 -0700

    complete lab demo

---
+++ ListyCity/app/src/main/java/com/example/listycity/CityList.java
@@ -0,0 +1,47 @@
+package com.example.listycity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This is a class that keeps a list of city objects
+ */
+public class CityList {
+    private List<City> cities = new ArrayList<>();
+
+    /**
+     * This adds a city to the list if the city does not exist
+     * @param city
+     * This is a candidate city to add
+     */
+    public void add(City city) {
+        if (cities.contains(city)) {
+            throw new IllegalArgumentException();
+        }
+        cities.add(city);
+    }
+
+    /**
+     * This returns a sorted list of cities
+     * @return
+     * Return the sorted list
+     */
+    public List<City> getCities() {
+        List<City> list = cities;
+        Collections.sort(list);
+        return list;
+    }
+
+    /**
+     * Checks whether the given city is in the list or not
+     * @param city
+     * @return true if the city is in the list; false if not
+     */
+    public boolean hasCity(City city) {
+        if (cities.contains(city)) {
+            return true;
+        }
+        return false;
+    }
+}
